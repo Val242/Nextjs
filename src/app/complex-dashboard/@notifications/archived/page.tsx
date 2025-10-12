53d68d8400b03ad566d9f8878ef6a15ea705c950
=Val242 <=ebongvalentine70@gmail.com>
2025-10-12 23:53:08 +0100

    Parallel and Intercepted Routes

---
+++ src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -0,0 +1,17 @@
+import { Card } from "@/components/card";
+import Link from "next/link";
+
+export default function ArchivedNotifications(){
+    return (
+    <Card>
+        <div>Archived notifications</div>
+
+            <div>
+                <Link href="/complex-dashboard/">Default</Link>
+            </div>
+
+    </Card>
+
+
+)
+}
